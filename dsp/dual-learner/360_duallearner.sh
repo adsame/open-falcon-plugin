82681f44c609046bab88de8385bb0500e0b3af94
ymyjohnny@adsame.com <ymyjohnny@adsame.com>
2015-07-14 21:15:35 +0800

    dual-learner

--- dsp/dual-learner/360_duallearner.sh
+++ dsp/dual-learner/360_duallearner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-date=`date -d -10min +%F' '%H:%M`
+date=`date -d -10min +%s`
 
 dual=`cat /tmp/dual|tail -1 |awk '{print $1}'`
 
@@ -8,8 +8,9 @@ dual=`cat /tmp/dual|tail -1 |awk '{print $1}'`
 
 
 if [ $dual -gt 0 ];then
-  echo dualview is ok > /dev/null
-  echo "[{\"endpoint\": \"$hostname\", \"tags\": \"\", \"timestamp\":$date, \"metric\": \"dual-learner\", \"value\": 1, \"counterType\": \"GAUGE\", \"step\": 60}]"
+#  echo dualview is ok > /dev/null
+  echo "[{\"endpoint\": \"$hostname\", \"tags\": \"\", \"timestamp\":$date, \"metric\": \"duallearner\", \"value\": 1, \"counterType\": \"GAUGE\", \"step\": 60}]"
 else
-  echo "[{\"endpoint\": \"$hostname\", \"tags\": \"\", \"timestamp\":$date, \"metric\": \"dual-learner\", \"value\": 0, \"counterType\": \"GAUGE\", \"step\": 60}]"
+  echo "[{\"endpoint\": \"$hostname\", \"tags\": \"\", \"timestamp\":$date, \"metric\": \"duallearner\", \"value\": 0, \"counterType\": \"GAUGE\", \"step\": 60}]"
 fi
+
